b4a7df20fad793dcd606740a9bd438186dc08a90
jurgengalicia <jurgen.galicia@gmail.com>
2023-01-20 23:11:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var tel = document.getElementById("tel-input")
 
 tel.addEventListener('keydown', function(e){
-    if(e.key != "Backspace" && (tel.value.length === 3 || tel.value.length === 7)){
+    if(e.key != "Backspace" && (tel.value.length === 3 || tel.value.length === 7))
         tel.value += "-";
-    }
+    
 })
