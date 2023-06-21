317d16c6312ab9a1683092cc6192888d72b51b4d
jurgengalicia <jurgen.galicia@gmail.com>
2023-06-20 22:10:10 -0700

    password validity

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 var tel = document.getElementById("tel-input")
+var password = document.getElementsByClassName("pass")
+var conf_password = document.getElementsByClassName("conf-pass")
 
 tel.addEventListener('keydown', function(e){
     if(e.key != "Backspace" && (tel.value.length === 3 || tel.value.length === 7))
         tel.value += "-";
-    
 })
+
+function validatePassword(){
+    if(password.value != conf_password.value)
+        conf_password.setCustomValidity("Passwords do not match");
+    else
+        conf_password.setCustomValidity("");
+}
+
+password.onchange = validatePassword;
+conf_password.onchange = validatePassword;
