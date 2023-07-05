094c54fd0905d185bf47aede0729eb071175a627
jurgengalicia <jurgen.galicia@gmail.com>
2023-07-05 00:08:16 -0700

    quarantine erroneous function

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var tel = document.getElementById("tel-input")
 var password = document.getElementsByClassName("pass")
 var conf_password = document.getElementsByClassName("conf-pass")
 
-tel.addEventListener('keydown', function(e){
-    if(e.key != "Backspace" && (tel.value.length === 3 || tel.value.length === 7))
-        tel.value += "-";
-})
+// tel.addEventListener('keydown', function(e){
+//     if(e.key != "Backspace" && (tel.value.length === 3 || tel.value.length === 7))
+//         tel.value += "-";
+// })
 
 function validatePassword(){
     if(password.value != conf_password.value)
