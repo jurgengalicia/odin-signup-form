f071845dbee4d7638518b3f545e563214d07d1a2
jurgengalicia <jurgen.galicia@gmail.com>
2023-07-06 12:20:21 -0700

    phones id adjust

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var tel = document.getElementById("tel-input")
+var tel = document.getElementById("tel")
 var password = document.getElementsByClassName("pass")
 var conf_password = document.getElementsByClassName("conf-pass")
 
-// tel.addEventListener('keydown', function(e){
-//     if(e.key != "Backspace" && (tel.value.length === 3 || tel.value.length === 7))
-//         tel.value += "-";
-// })
+tel.addEventListener('keydown', function(e){
+    if(e.key != "Backspace" && (tel.value.length === 3 || tel.value.length === 7))
+        tel.value += "-";
+})
 
 function validatePassword(){
     if(password.value != conf_password.value)
